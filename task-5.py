10a83710745c147369f424d2a3e24e9dc33fe6b7
preksha2208 <prekshashah025@gmail.com>
2019-05-29 14:37:18 -0400

    task-5

---
+++ task-5.py
@@ -0,0 +1,11 @@
+#  DEFINE DATA STRUCTURES TO REPRESENT SEXPRS. (10L) 
+class sexpr:
+   def emptystring(self):
+      empty_tuple = () 
+      print (empty_tuple)
+   def strings(self):
+      strng = "sexpr"
+      print(strng)
+   def pair(self):
+      tup1 = ('string1', 'string2')
+      print(tup1)
