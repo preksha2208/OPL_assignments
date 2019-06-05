992d5a23cf291a4de700906d7bada2b911ee69b6
preksha2208 <prekshashah025@gmail.com>
2019-06-05 02:21:07 -0400

    task-12

---
+++ task-12.py
@@ -0,0 +1,10 @@
+#  DEFINE DATA STRUCTURES TO REPRESENT CONTEXT. (9L) 
+class type1 :
+    def __init__(self,list1):
+        self.list1 = list1
+class type2:
+    def __init__(self,list2):
+      self.list2 = list2
+class type3:
+    def __init__(self,list3):
+      self.list3 = list3
