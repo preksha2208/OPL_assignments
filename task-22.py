772f49f0a654cd291b26775aa8bed6064e4f1225
preksha2208 <prekshashah025@gmail.com>
2019-06-27 00:23:15 -0400

    task-22

---
+++ task-22.py
@@ -0,0 +1,26 @@
+# IMPLEMENT THE CK0 MACHINE INTERPRETER FOR J1.(25L)
+class interpreter :
+    def __init__ (self,temp_list):
+        self.temp_list = temp_list
+    def __str__(self):
+        if (self.temp_list[0] == 'TRUE' ):
+            return ( "[ " + str(self.temp_list[0]) + " " + "," + " " + str(self.temp_list[1])  + " " + " if [ " + str(self.temp_list[2])  + " " + str(self.temp_list[3]) + " " + str(self.temp_list[4]) + " ]" + " ]"  )
+        if (self.temp_list[0] == 'FALSE'):
+            return ( "[ " + str(self.temp_list[0]) + " " + "," + " " + str(self.temp_list[1])  + " " + " if [" + str(self.temp_list[2])  + " " + str(self.temp_list[3]) + " " + str(self.temp_list[4]) + " ]" + " ]"  )
+        if(self.temp_list[4] == 'K'):
+            return ( "[" +str(self.temp_list[0]) + " " + str(self.temp_list[1])  + " " + str(self.temp_list[2])  + " " + str(self.temp_list[3]) + " " + str(self.temp_list[4]) + " ]"   )
+    def interp(self):
+        if(self.temp_list[0] == 'if'):
+            return ( "[ " + "C"+str(self.temp_list[1]) + " " + "," + " " + str(self.temp_list[4])  + "if (" + " " + str(self.temp_list[2]) + " " + str(self.temp_list[3]) + " " + str(self.temp_list[4]) +" )" + " ]"  )    
+        elif (self.temp_list[0] == 'TRUE'):
+                return("[ " + str(self.temp_list[2]) + " , " + str(self.temp_list[1]) + " ]" )
+        elif (self.temp_list[0] == 'FALSE'):
+                return("[ " + str(self.temp_list[3]) + " , " + str(self.temp_list[1]) + " ]" )    
+print(interpreter(["if","2 < 5","TRUE then 2","FALSE then 5","K"]))
+print(interpreter(["if","2 < 5","TRUE then 2","FALSE then 5","K"]).interp())
+print("\n")
+print(interpreter(["TRUE","K","SATISFIED","NOT-SATISFIED","K"]))
+print(interpreter(["TRUE","K","SATISFIED","NOT-SATISFIED","K"]).interp())
+print("\n")
+print(interpreter(["FALSE","K","SATISFIED","NOT-SATISFIED","K"]))
+print(interpreter(["FALSE","K","SATISFIED","NOT-SATISFIED","K"]).interp())
