31b76b73e08d2cdd9c17fea1d1d2192b2e8b97e0
preksha2208 <prekshashah025@gmail.com>
2019-06-30 03:06:29 -0400

    task-24 HL

---
+++ task-24H.py
@@ -0,0 +1,19 @@
+#  DEFINE DATA STRUCTURES TO REPRESENT J2 IN HIGH LEVEL. (19L) 
+class E :
+    def __init__(self,lists):
+        self.lists = lists
+class V:
+    def __init__(self,val):
+        self.val = val
+class prim:
+    def __init__(self,oper):
+        self.oper = oper
+class X :
+    def __init__ (self,varnam)
+        self.varnam = varnam
+class F :
+    def __init__(self,func)
+        self.func = func
+class D :
+    def __init(self,defi)
+        self.defi = defi
