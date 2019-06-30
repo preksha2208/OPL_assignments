9e0c38bbf724e9b94fa47a81e72e6e148a5b97f7
preksha2208 <prekshashah025@gmail.com>
2019-06-30 03:14:03 -0400

    task-24 LL

---
+++ task-24L.c
@@ -0,0 +1,35 @@
+// DEFINE DATA STRUCTURE TO REPRESENT J1 PROGRAMS. (20L) 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+struct E {
+   char  tag[50];
+}; 
+struct V {
+   int number ;
+   bool tf ; 
+   char  prim[50];
+};
+struct primitive {
+   char  sym[50];
+}; 
+struct X {
+   char  varnam[50];
+}; 
+struct F {
+   char  func[50];
+}; 
+struct D {
+   char  defi[50];
+}; 
+
+ void main( ) 
+{
+   struct E obje;        
+   struct V objv;
+   struct primitive objp;
+   struct X objx;        
+   struct F objf;
+   struct D objd;
+   
+}  
