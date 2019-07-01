a596b69c899de0671263e7ddb59b0676ab26f18e
preksha2208 <prekshashah025@gmail.com>
2019-07-01 18:27:04 -0400

    task-34 low level

---
+++ task-34L.c
@@ -0,0 +1,30 @@
+// DEFINE DATA STRUCTURE TO REPRESENT J1 PROGRAMS. (20L) 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define temp  3
+struct E {
+   char  tag[10];
+   char  exp[10];
+   int num1;
+   int num2;
+}item[temp];
+; 
+struct V {
+   int number ;
+   bool tf ; 
+   char  prim[50];
+};
+struct primitive {
+   char  sym[50];
+}; 
+struct X {
+   char  varnam[50];
+}; 
+ void main( ) 
+{        
+   struct V objv;
+   struct primitive objp;
+   struct X objx;        
+   
+}  
