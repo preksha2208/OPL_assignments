e998b4ef027a7837e0c93ce885e982f3407984a1
preksha2208 <prekshashah025@gmail.com>
2019-07-01 20:24:48 -0400

    task-39 high level

---
+++ task-39H.py
@@ -0,0 +1,13 @@
+#  DEFINE DATA STRUCTURES TO REPRESENT J4 IN HIGH LEVEL. (13L) 
+class E :
+    def __init__(self,lists):
+        self.lists = lists
+class V:
+    def __init__(self,val):
+        self.val = val
+class prim:
+    def __init__(self,oper):
+        self.oper = oper
+class X :
+    def __init__ (self,varnam):
+        self.varnam = varnam
