aa3ec124420cc1806e26826b6f345366a0cfd84a
preksha2208 <prekshashah025@gmail.com>
2019-07-01 20:26:43 -0400

    task-39 low level

---
+++ task-39L.c
@@ -0,0 +1,29 @@
+// DEFINE DATA STRUCTURE TO REPRESENT J4 PROGRAMS. (28L) 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define temp  3
+struct E {
+   char  tag[10];
+   char  exp[10];
+   int num1;
+   int num2;
+}item[temp];
+; 
+struct V {
+   int number ;
+   bool tf ; 
+   char  prim[50];
+};
+struct primitive {
+   char  sym[50];
+}; 
+struct X {
+   char  varnam[50];
+}; 
+ void main( ) 
+{        
+   struct V objv;
+   struct primitive objp;
+   struct X objx;         
+}  
